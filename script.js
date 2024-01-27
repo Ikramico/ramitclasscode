470425c0585951f3ae711746b27185c9c9f98bdd
Ikram Ico <ikramicono@gmail.com>
2024-01-27 19:06:49 +0600

    all code

---
+++ script.js
@@ -0,0 +1,9 @@
+// alert('This is Javascript');
+
+let x = "this is javascript's string";
+let a = 19
+for(let i=1; i<50;i=i*3 ){
+    console.log(`Multiplication of ${a}*${i} = ${19*i}`);
+}
+
+console.log(i);
